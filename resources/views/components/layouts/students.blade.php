0ce7784199d638cf3551be6c0ae93bbd10a2d230
brn2bwild <brn_2_b_wild@hotmail.com>
2024-02-17 00:34:38 -0600

    Modificadas las direcciones los botones del navbar del layout students.

--- resources/views/components/layouts/students.blade.php
+++ resources/views/components/layouts/students.blade.php
@@ -10,6 +10,7 @@
 </head>
 
 <body class="antialiased bg-neutral-50 min-h-screen flex flex-col items-center">
+	<x-toast />
 	<nav class="w-full flex justify-between bg-blue-950 p-4 items-center">
 		<div>
 			<img src="/storage/images/logo_tecnm_blanco.png" class=" h-16" alt="logo_tecnm">
@@ -17,9 +18,9 @@
 		<div class="flex gap-8">
 			<a href="{{ route('home') }}" wire:navigate
 				class="rounded-xl border-2 border-neutral-50 px-4 py-2 font-medium text-neutral-50">Inicio</a>
-			<a href="{{ route('home') }}" wire:navigate
+			<a href="{{ route('students.register') }}" wire:navigate
 				class="rounded-xl bg-neutral-50 text-blue-950 px-4 py-2 font-medium">Registro</a>
-			<a href="{{ route('home') }}" wire:navigate
+			<a href="{{ route('students.certificate') }}" wire:navigate
 				class="rounded-xl bg-neutral-50 text-blue-950 px-4 py-2 font-medium">Constancias</a>
 		</div>
 	</nav>
