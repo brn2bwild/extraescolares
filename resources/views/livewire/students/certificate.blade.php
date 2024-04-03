654c6d23056563415a759465475e280c1a6addda
brn2bwild <brn_2_b_wild@hotmail.com>
2024-04-03 13:08:16 -0600

    Modificados los estilos de las vistas registro y constancias.

--- resources/views/livewire/students/certificate.blade.php
+++ resources/views/livewire/students/certificate.blade.php
@@ -10,7 +10,7 @@
 			</div>
 			<div class="w-full flex justify-center mt-8">
 				<button type="submit"
-					class="bg-neutral-50 rounded-xl px-4 py-2 font-medium text-blue-950 flex justify-between items-center">
+					class="bg-neutral-50 rounded-xl text-lg px-4 py-2 font-medium text-blue-950 flex justify-between items-center">
 					<img wire:loading src="/storage/images/spinner.svg" alt="spinner" class="h-6 mr-2">
 					Consultar
 				</button>
