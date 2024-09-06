bfd012282340160641c2dfb4671ad24bd07945b5
brn2bwild <brn_2_b_wild@hotmail.com>
2024-09-06 12:04:56 -0600

    Actualizadas las dependencias. Agregada la autorización para los páneles en el modelo usuario

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,6 +3,8 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Filament\Models\Contracts\FilamentUser;
+use Filament\Panel;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
@@ -13,48 +15,55 @@ use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 use Spatie\Permission\Traits\HasRoles;
 
-class User extends Authenticatable
+class User extends Authenticatable implements FilamentUser
 {
-	use HasApiTokens, HasFactory, Notifiable, HasRoles;
+    use HasApiTokens, HasFactory, Notifiable, HasRoles;
 
-	/**
-	 * The attributes that are mass assignable.
-	 *
-	 * @var array<int, string>
-	 */
-	protected $fillable = [
-		'name',
-		'email',
-		'password',
-	];
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = ["name", "email", "password"];
 
-	/**
-	 * The attributes that should be hidden for serialization.
-	 *
-	 * @var array<int, string>
-	 */
-	protected $hidden = [
-		'password',
-		'remember_token',
-	];
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array<int, string>
+     */
+    protected $hidden = ["password", "remember_token"];
 
-	/**
-	 * The attributes that should be cast.
-	 *
-	 * @var array<string, string>
-	 */
-	protected $casts = [
-		'email_verified_at' => 'datetime',
-		'password' => 'hashed',
-	];
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        "email_verified_at" => "datetime",
+        "password" => "hashed",
+    ];
 
-	public function activities(): BelongsToMany
-	{
-		return $this->BelongsToMany(Activity::class);
-	}
+    public function canAccessPanel(Panel $panel): bool
+    {
+        if ($panel->getId() === "admin") {
+            // return str_ends_with($this->email, '@yourdomain.com') && $this->hasVerifiedEmail();
+            return $this->hasRole("admin");
+        }
 
-	public function getActivitiesIds(): array
-	{
-		return $this->activities->pluck('id')->toArray();
-	}
+        if ($panel->getId() === "teacher") {
+            return $this->hasRole("teacher");
+        }
+
+        return true;
+    }
+
+    public function activities(): BelongsToMany
+    {
+        return $this->BelongsToMany(Activity::class);
+    }
+
+    public function getActivitiesIds(): array
+    {
+        return $this->activities->pluck("id")->toArray();
+    }
 }
